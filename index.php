92e5e2fb2d74e198817b5353024fcee43e4024f7
mouadali <mouadalli311@gmail.com>
2025-12-03 12:47:48 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-    define('ROOT_PATH' , __DIR__);
 
-    require_once ROOT_PATH . '/router/router.php';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-?>
+
+$routes = [
+    '/'          => 'controllers/home.php',
+    '/services'  => 'controllers/services.php',
+    '/about'     => 'controllers/about.php',
+    '/contact'   => 'controllers/contact.php',
+];
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+    require 'controllers/404.php';
+    exit;
+}
