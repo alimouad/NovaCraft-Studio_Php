d10a1ceaabcdf3608f3b01dea845cf61ba7d5168
mouadali <mouadalli311@gmail.com>
2025-12-04 11:32:59 +0100

    add router fodler

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/' . 'router/router.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+route($uri);
 
-$routes = [
-    '/'          => 'controllers/home.php',
-    '/services'  => 'controllers/services.php',
-    '/about'     => 'controllers/about.php',
-    '/contact'   => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-    require 'controllers/404.php';
-    exit;
-}
+?>
